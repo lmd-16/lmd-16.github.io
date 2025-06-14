81694ab17467e7f85332588d0fca148d9d96f711
lmd-16 <dleeannmarie07@gmail.com>
2025-06-14 16:01:46 -0700

    Added github links to projects

--- miniproj/src/pages/Projects.jsx
+++ miniproj/src/pages/Projects.jsx
@@ -8,11 +8,15 @@ const Projects = () => {
             className="h-screen bg-cover bg-center flex items-center justify-center"
             style={{ backgroundImage: `url(${background})` }}
         >
-        
-
-            <button class="absolute top-[300px] left-[330px] btn btn-accent px-4 py-2 rounded btn-bubble">Bubble Sort Visualizer</button> 
-            <button class="absolute top-[600px] left-[650px] btn btn-accent px-4 py-2 rounded btn-glag">AVL Tree Word Sorter </button>
-            <button class="absolute top-[520px] left-[1250px] btn btn-accent px-4 py-2 rounded btn-avl ">Guess the Flag</button>
+            <a href="https://github.com/lmd-16/Bubble-Sort-Visualizer" target="_blank" rel="noopener noreferrer">                
+                <button class="absolute top-[300px] left-[330px] btn btn-accent px-4 py-2 rounded btn-bubble">Bubble Sort Visualizer</button> 
+            </a>
+            <a href="https://github.com/lmd-16/AVL-Tree-Word-Counter"target="_blank" rel="noopener noreferrer">
+                <button class="absolute top-[600px] left-[650px] btn btn-accent px-4 py-2 rounded btn-glag">AVL Tree Word Sorter </button>
+            </a>
+            <a href="https://github.com/lmd-16/Guess-the-flag/tree/main/Desktop/HD3/Guess"target="_blank" rel="noopener noreferrer">
+                <button class="absolute top-[520px] left-[1250px] btn btn-accent px-4 py-2 rounded btn-avl ">Guess the Flag</button>
+            </a>
             <Link to="/About">About
                 <button class="absolute top-[90px] left-[30px] px-4 py-2 rounded btn btn-error btn-home">Home</button>
             </Link>
