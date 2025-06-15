f5966ca34beae871a849e512b1ce5e144742d236
lmd-16 <dleeannmarie07@gmail.com>
2025-06-14 18:21:04 -0700

    Added Main Layout

---
+++ miniproj/src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+
+const MainLayout = () => {
+    return (
+        <>
+        <Outlet/>
+        {/* <Navbar /> */}
+        </>
+    )
+};
+
+export default MainLayout
