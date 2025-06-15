02f82a6f5fba5b9b2e3e7983072cc6056c16511b
lmd-16 <dleeannmarie07@gmail.com>
2025-06-14 21:26:44 -0700

    Added inspiration page and image

--- miniproj/src/App.jsx
+++ miniproj/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from './layouts/MainLayout';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import Insp from './pages/Insp';
 
 function App() {
   return(
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/About" element={<About />}/>
         <Route path="/Projects" element={<Projects />}/>
+        <Route path="/Insp" element={<Projects />}/>
 
     </Route>
   </Routes>
--- miniproj/src/pages/Insp.jsx
+++ miniproj/src/pages/Insp.jsx
@@ -1,8 +1,9 @@
-import background from '../images/bg6.png';
+import React from 'react'
+import background from '../images/bg6.png'
 import { Link } from 'react-router-dom';
-import Pdf from '../assets/Leeann_D_Resume.pdf'
 
-const Projects = () => {
+
+const Insp = () => {
     return (
         <>
         <div class="navbar bg-black">
@@ -40,10 +41,6 @@ const Projects = () => {
                     <a href={Pdf} target="_blank" rel="noopener noreferrer">
                     <li><a>Resume</a></li>
                     </a>
-                    <Link to="/Insp">
-                    <li><a className= "text-xs">Inspiration</a></li>
-                    </Link>
-
                 </ul>
                 </details>
             </li>
@@ -54,20 +51,41 @@ const Projects = () => {
             className="h-screen bg-cover bg-center flex items-center justify-center"
             style={{ backgroundImage: `url(${background})` }}
         >
-            <img
-            src={background}
-            alt="Celeste Chapter 2: Old Site background"
-            className="hidden"
-            />
+        <div>
+        <div
+            className="h-screen bg-cover bg-center flex items-center justify-center"
+            style={{ backgroundImage: `url(${background})`}}
             
-            <a href="https://github.com/lmd-16/Guess-the-flag/tree/main/Desktop/HD3/Guess"target="_blank" rel="noopener noreferrer">
-                <button class="absolute top-[620px] left-[1150px] btn btn-accent px-4 py-2 rounded btn-avl ">Guess the Flag</button>
+        >
+        <img
+        src={background}
+        alt="Celeste Chapter 3: Old Site background"
+        className="hidden"
+        />
+        <div className="font-pixel text-xs text-center text-blue-300 absolute top-[300px] left-[230px] w-80 h-50 overflow-y-scroll bg-blue p-4">I’m Leeann, a computer science student at Simon Fraser University with a strong mathematics background and a growing interest in the intersection of math and computer science. I’m currently working on exploring different avenues, 
+            such as data science and machine learning. Click the link to see some fun projects!
+        </div>
+        <div>
+            <a href={Pdf} target="_blank" rel="noopener noreferrer">
+                <button class="absolute top-[350px] left-[940px] btn btn-accent px-4 py-2 rounded btn-resume ">Resume</button>
             </a>
-            <footer class="text-[6px] text-gray-500 absolute top-[760px] left-[730px]">Background image from https://www.ign.com/wikis/celeste/Chapter_2-_Old_Site
-            </footer>        
-            </div>
-        </>
-    );
-};
+            <Link to="/Projects">
+                <button class="absolute top-[250px] left-[930px] btn btn-accent px-4 py-2 rounded btn-projects ">Projects</button>
+            </Link>
+            <Link to="/">
+                <button class="absolute top-[40px] left-[120px] btn btn-outline btn-accent px-4 py-2 rounded btn-home">Home</button>  
+    
+            </Link>
+        </div>
+        </div>
+        <footer class="text-[6px] text-gray-500 absolute top-[760px] left-[930px]">Background image from https://www.ign.com/wikis/celeste/Chapter_3-_Celestial_Resort
+        </footer>        
 
-export default Projects
+            
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Insp
