541d86223767dc5b61d10ff11ed374b50de7f574
lmd-16 <dleeannmarie07@gmail.com>
2025-06-14 17:59:59 -0700

    Added footer for homepage

--- miniproj/src/components/Welcome.jsx
+++ miniproj/src/components/Welcome.jsx
@@ -14,8 +14,9 @@ const Welcome = () => {
     </Link>
     </div>
     </div>
+    <footer class="text-xs text-gray-500 absolute top-[760px] left-[930px]">Background image from https://www.ign.com/wikis/celeste/Chapter_1-_Forsaken_City
+    </footer>        
     </>
-
 
     );
 };
